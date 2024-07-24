73a2d9eca1d4234d5dfca486e143efcce80965d3
Poi5eN <111029389+Poi5eN@users.noreply.github.com>
2024-07-24 15:41:04 +0530

    Update classModel.js

--- models/classModel.js
+++ models/classModel.js
@@ -1,26 +1,19 @@
 const mongoose = require('mongoose');
 
 const classSchema = new mongoose.Schema({
-  schoolId: {
-    type: String,
-    required: true
-  },
-  className: {
-    type: String,
-    required: true
-  },
-  section: {
-    type: [String],
-    default: []
-  },
-  subject: {
-    type: [String],
-    default: []
-  },
-  primary: {
-    type: Boolean,
-    default: false
-  }
+    schoolId : {
+        type: String,
+        required: true
+    },
+    className: {
+        type: String,
+        required: true
+    },
+    section :[String],
+    subject :[String],
+    primary: {
+        type: Boolean,
+        default: false
+    }
 })
-// Check if the model already exists to avoid OverwriteModelError
-module.exports = mongoose.models.Class || mongoose.model('Class', classSchema);
+module.exports = mongoose.model('Class', classSchema)
