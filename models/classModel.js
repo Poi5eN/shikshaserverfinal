5fecf9e8eea8a5a9121b65a796ff2b24ad9288dc
Poi5eN <111029389+Poi5eN@users.noreply.github.com>
2024-07-24 15:25:48 +0530

    Update classModel.js

--- models/classModel.js
+++ models/classModel.js
@@ -1,26 +1,25 @@
 const mongoose = require('mongoose');
 
 const classSchema = new mongoose.Schema({
-    schoolId : {
-        type: String,
-        required: true
-    },
-    className: {
-        type: String,
-        required: true
-    },
-    section: {
-        type: [String],
-        default: []
-    },
-    subject: {
-        type: [String],
-        default: []
-    },
-    primary: {
-        type: Boolean,
-        default: false
-    }
-});
+  schoolId: {
+    type: String,
+    required: true
+  },
+  className: {
+    type: String,
+    required: true
+  },
+  section: {
+    type: [String],
+    default: []
+  },
+  subject: {
+    type: [String],
+    default: []
+  },
+  primary: {
+    type: Boolean,
+    default: false
+  }
 })
 module.exports = mongoose.model('Class', classSchema)
