3ffd96a3fc5b547b7ff41981a783d25b256ef544
Poi5eN <111029389+Poi5eN@users.noreply.github.com>
2024-07-31 15:27:04 +0530

    Update multer.js multer as per bulk updated

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,7 +1,31 @@
-const multer = require('multer')
-const storage = multer.memoryStorage()
+// const multer = require('multer')
+// const storage = multer.memoryStorage()
 
-const singleUpload = multer({storage}).single('image')
-const uploads = multer({storage}).any()
+// const singleUpload = multer({storage}).single('image')
+// const uploads = multer({storage}).any()
 
-module.exports = {singleUpload, uploads}
+// module.exports = {singleUpload, uploads}
+
+
+const multer = require('multer');
+const path = require('path');
+
+// Disk storage configuration
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/'); // Directory for storing files
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname)); // Adding timestamp to the file name
+  }
+});
+
+const upload = multer({ storage });
+
+// Middleware to handle single file uploads
+const singleUpload = upload.single('image');
+
+// Middleware to handle multiple files uploads
+const uploads = upload.any();
+
+module.exports = { singleUpload, uploads };
