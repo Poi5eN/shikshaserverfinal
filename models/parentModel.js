df6226dedcf872b7fad2e53ba3b5a71baaffe4e3
Poi5eN <111029389+Poi5eN@users.noreply.github.com>
2024-08-01 20:05:22 +0000

    req remove

--- models/parentModel.js
+++ models/parentModel.js
@@ -48,11 +48,11 @@ const parentSchema = new mongoose.Schema({
     image: {
         public_id: {
             type: String,
-            required: true
+            // required: true
         },
         url: {
             type: String,
-            required: true
+            // required: true
         }
     },
     admissionNumber: {
