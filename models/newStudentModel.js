1701e308f045531ee0c8c213d2f65c8d97e5f208
Poi5eN <111029389+Poi5eN@users.noreply.github.com>
2024-08-06 08:50:38 +0000

    roll no unique remove

--- models/newStudentModel.js
+++ models/newStudentModel.js
@@ -48,7 +48,7 @@ const studentSchema = new mongoose.Schema({
     rollNo: {
         type: Number,
         // required: true,
-        unique: true,
+        // unique: true,
     },
     "parentId": {
         type: String,
